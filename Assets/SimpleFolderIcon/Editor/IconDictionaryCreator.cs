12e44b60c5823b5992c3324cd547bf83aa28dc1f
BorisTV <boristlkn@gmail.com>
2023-03-22 23:32:28 +0300

    add ability to change asset location without path editing

--- Assets/SimpleFolderIcon/Editor/IconDictionaryCreator.cs
+++ Assets/SimpleFolderIcon/Editor/IconDictionaryCreator.cs
@@ -1,13 +1,17 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
+
 namespace SimpleFolderIcon.Editor
 {
     public class IconDictionaryCreator : AssetPostprocessor
     {
-        private const string AssetsPath = "SimpleFolderIcon/Icons";
+        private const string AssetName = "SimpleFolderIcon";
         internal static Dictionary<string, Texture> IconDictionary;
+
+        private static string _iconsPath;
 
         private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
         {
@@ -26,8 +30,7 @@ namespace SimpleFolderIcon.Editor
         {
             foreach (string str in assets)
             {
-
-                if (ReplaceSeparatorChar(Path.GetDirectoryName(str)) == "Assets/" + AssetsPath)
+                if (ReplaceSeparatorChar(Path.GetDirectoryName(str)) == _iconsPath)
                 {
                     return true;
                 }
@@ -42,17 +45,38 @@ namespace SimpleFolderIcon.Editor
 
         internal static void BuildDictionary()
         {
+            _iconsPath = UpdateIconsPath();
+            
             var dictionary = new Dictionary<string, Texture>();
-
-            var dir = new DirectoryInfo(Application.dataPath + "/" + AssetsPath);
+            
+            var dir = new DirectoryInfo(Application.dataPath.Substring(0, Application.dataPath.LastIndexOf('/')) + "/" + _iconsPath);
             FileInfo[] info = dir.GetFiles("*.png");
             foreach(FileInfo f in info)
             {
-                var texture = (Texture)AssetDatabase.LoadAssetAtPath($"Assets/SimpleFolderIcon/Icons/{f.Name}", typeof(Texture2D));
+                var texture = (Texture)AssetDatabase.LoadAssetAtPath($"{_iconsPath}/{f.Name}", typeof(Texture2D));
                 dictionary.Add(Path.GetFileNameWithoutExtension(f.Name),texture);
             }
 
             IconDictionary = dictionary;
+        }
+        
+        private static string UpdateIconsPath()
+        {
+            var folders = new List<string>();
+            GetAllSubfolders("Assets", ref folders);
+            var path = folders.First(name => name.Substring((name.LastIndexOf('/') + 1)) == AssetName);
+            path += "/Icons";
+            return path;
+        }
+
+        private static void GetAllSubfolders(string startFolder, ref List<string> folders)
+        {
+            folders.Add(startFolder);
+            var subFolders = AssetDatabase.GetSubFolders(startFolder);
+            foreach (var subFolder in subFolders)
+            {
+                GetAllSubfolders(subFolder, ref folders);
+            }
         }
     }
 }
